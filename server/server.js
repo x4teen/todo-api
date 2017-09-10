f4d64da54501a09740f648bea19be936bb2fb721
x4teen <x4teen@yahoo.com>
2017-09-10 19:38:15 -0400

    completed applicaiton

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var ObjectID = require ('mongoose').Types.ObjectId;
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./model/todo');
 var {User} = require('./model/user');
@@ -32,12 +33,28 @@ app.get('/todos', (req, res) => {
     })
 }, (e) => {
     res.status(400).send(e);
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    //If ObjectID not valid send back error
+    if (!ObjectID.isValid(id)) {
+        res.status(400).send();
+    };
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return  res.status(404).send();
+        };
+        res.send(todo);
+    }).catch((e) => res.status(400).send());
+});
 
 
 app.listen(3000, ()=>{
     console.log('started on port 3000');
-})
+});
 
 
 
